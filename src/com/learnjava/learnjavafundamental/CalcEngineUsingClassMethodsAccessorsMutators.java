5e7f8a31c5628a772fe651a50e558bb0839043fb
Kamlesh <kamleshddobariya@yahoo.com>
2020-04-19 18:21:20 +0530

    initiate commit

---
+++ src/com/learnjava/learnjavafundamental/CalcEngineUsingClassMethodsAccessorsMutators.java
@@ -0,0 +1,33 @@
+package com.learnjava.learnjavafundamental;
+
+public class CalcEngineUsingClassMethodsAccessorsMutators {
+
+    public static void main(String[] args) {
+//        double[] leftVals = {100, 25, 225, 11};
+//        double[] rightVals = {50, 92, 17, 3};
+//        char[] opCodes = {'d', 'a','s', 'm'};
+//        double[] results = new double[opCodes.length];
+
+        MathEquation[] equations = new MathEquation[4];
+        equations[0] = create(100,50,'d');
+        equations[1] = create(25,92,'a');
+        equations[2] = create(225,17,'s');
+        equations[3] = create(11,3,'m');
+
+        for(MathEquation equation : equations) {
+
+            equation.execute();
+            System.out.print("result = ");
+            System.out.println(equation.result);
+        }
+    }
+
+    public static MathEquation create(double leftVal, double rightVal, char opCode) {
+        MathEquation equation = new MathEquation();
+        equation.leftVal = leftVal;
+        equation.rightVal = rightVal;
+        equation.opCode = opCode;
+
+        return equation;
+    }
+}
