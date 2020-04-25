3b4dd39a4ead2d3f311334c2bb9ac49d5bcff2cb
Kamlesh <kamleshddobariya@yahoo.com>
2020-04-25 09:47:39 +0530

    initiate commit

---
+++ src/com/learnjava/learnjavafundamental/CalcEngineUsingString.java
@@ -0,0 +1,27 @@
+package com.learnjava.learnjavafundamental;
+
+public class CalcEngineUsingString {
+
+    public static void main(String[] args) {
+
+
+        String[] statements;
+        statements = new String[]{
+                "divide 100.0 50.0",
+                "add 25.0 92.0",
+                "subtract 225.0 17.0",
+                "multiply 11.0 3.0",
+
+        };
+
+        CalculateHelper helper = new CalculateHelper();
+        for(String statement:statements) {
+            helper.process(statement);
+
+            System.out.println(helper);
+        }
+
+    }
+
+
+}
---
+++ src/com/learnjava/learnjavafundamental/MathCommand.java
@@ -0,0 +1,10 @@
+package com.learnjava.learnjavafundamental;
+
+public enum MathCommand {
+
+    Add,
+    Subtract,
+    Multiply,
+    Divide
+
+}
